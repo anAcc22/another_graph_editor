68a3bc66baff6b690f52b6f4194b2a003bc422ed
anacc22 <a3jd2cwln@gmail.com>
2024-08-22 07:18:10 +0800

    fix: increase z-index of github button

--- src/App.tsx
+++ src/App.tsx
@@ -60,7 +60,7 @@ function App() {
         <a
           className="flex sm:top-2 lg:top-2 sm:right-2 lg:right-2 absolute
             border-2 border-border rounded-lg px-2 py-1 justify-between
-            items-center hover:border-border-hover"
+            items-center hover:border-border-hover z-10"
           href="https://github.com/anAcc22/another_graph_editor"
         >
           {settings.darkMode ? (

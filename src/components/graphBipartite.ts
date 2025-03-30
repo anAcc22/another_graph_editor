ccbef54b3330b8c39fadd804f81f082faccb441e
anacc22 <a3jd2cwln@gmail.com>
2025-03-30 21:43:01 +0800

    fix: use undirected adjacency list in bipartite coloring (#22)

--- src/components/graphBipartite.ts
+++ src/components/graphBipartite.ts
@@ -6,11 +6,25 @@ export function buildBipartite(
 ): [boolean, ColorMap, LayerMap] {
   let colorMap: ColorMap = new Map<string, number>();
   let layerMap: LayerMap = new Map<string, Layer>();
+  
+  const adjFull = new Map<string, string[]>();
+
+  for (const u of nodes) {
+    adjFull.set(u, []);
+  }
+
+  for (const u of nodes) {
+    for (const v of adj.get(u)!) {
+      adjFull.set(u, [...adjFull.get(u)!, v]);
+      adjFull.set(v, [...adjFull.get(v)!, u]);
+    }
+  }
+  console.log(adjFull);
 
   let okay = true;
 
   const dfs = (u: string): void => {
-    for (const v of adj.get(u)!) {
+    for (const v of adjFull.get(u)!) {
       if (!colorMap.has(v)) {
         colorMap.set(v, colorMap.get(u) === 1 ? 2 : 1);
         layerMap.set(v, [layerMap.get(u)![0] === 1 ? 2 : 1, 2]);

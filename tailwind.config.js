4387d274c25bb22a81849d5507c0a06082b8b03a
anAcc22 <a3jd2cwln@gmail.com>
2025-05-31 11:51:52 +0800

    fix: tweak sizing to prevent text overflow

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,11 @@ export default {
     screens: {
       sm: "0px",
 
-      md: "768px",
+      md: "800px",
 
-      lg: "1024px",
+      lg: "1200px",
 
-      xl: "1280px",
+      xl: "1420px",
     },
     extend: {
       spacing: {
